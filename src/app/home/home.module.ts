020c12380167c6aedfcdbe4d8fce54fb5d866870
Paulo Júnior <pauloafpjunior@hotmail.com>
2021-03-04 14:31:23 -0300

    Add home page initial layout

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,15 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { CityDetailsComponent } from './components/city-details/city-details.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    HomePageRoutingModule
-  ],
-  declarations: [HomePage]
+  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
+  declarations: [HomePage, CityDetailsComponent],
 })
 export class HomePageModule {}
