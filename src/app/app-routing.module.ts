af8a9e381c5a80e6c25516cd0e401528f7cd194f
Paulo Júnior <pauloafpjunior@hotmail.com>
2021-03-04 21:43:52 -0300

    Add weather page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'weather',
+    loadChildren: () => import('./weather/weather.module').then( m => m.WeatherPageModule)
+  },
 ];
 
 @NgModule({
