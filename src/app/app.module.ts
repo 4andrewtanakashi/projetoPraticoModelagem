27b10863a4b2e841460cd0896e7c06d163112007
Paulo Júnior <pauloafpjunior@hotmail.com>
2021-03-08 08:25:13 -0300

    Add search city by name service factory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,22 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SearchCityByNameService } from 'src/domain/services/search-city-by-name.service';
+import { FakeCityRepository } from 'src/data/fake/fake-city-repository';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    {
+      provide: SearchCityByNameService,
+      useFactory: () => {
+        return new SearchCityByNameService(new FakeCityRepository());
+      },
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
