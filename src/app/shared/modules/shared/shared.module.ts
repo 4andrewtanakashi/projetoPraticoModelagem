c5f41e0755cb02892936c916766aed5671ec8a94
Paulo Júnior <pauloafpjunior@hotmail.com>
2021-03-15 18:16:25 -0300

    Add Ionic Module to shared module

--- src/app/shared/modules/shared/shared.module.ts
+++ src/app/shared/modules/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorMessageComponent } from '../../components/error-message/error-message.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [ErrorMessageComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, IonicModule],
   exports: [ErrorMessageComponent],
 })
 export class SharedModule {}
