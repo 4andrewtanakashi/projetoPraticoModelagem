e40fb45c1cb237831d197ae9eb6e4e73ea0a5202
4andrewtanakashi <andrew.vita@estudante.ufla.br>
2021-05-02 14:56:05 -0300

    :wrench: Reparo no ativador de histórico II

--- src/domain/services/history-city.service.ts
+++ src/domain/services/history-city.service.ts
@@ -19,21 +19,16 @@ export class HistoryCityService {
     }
 
     async loadByCityies () : Promise<City[]> {
-        console.log("TESTE loadByCityies");
-        console.log("this._storage II: ", this._storage);
         let citiesAux = [];
         this._storage?.forEach( (key, value, index) => {
-            console.log("key 00", key);
             citiesAux.push(key);
         })
         .then( _ => {
-            console.log("cities II: ", citiesAux);
             return citiesAux;
         } )
         .catch( (error) => {
             return Promise.reject(error);
         });
-        console.log("citiesIV: ", citiesAux);
         return citiesAux;
     }
 }
